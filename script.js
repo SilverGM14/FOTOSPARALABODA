342c9825816fb77295e95c493f39a3374861d81c
SilverGM14 <161602519+SilverGM14@users.noreply.github.com>
2024-08-30 00:36:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('uploadBtn').addEventListener('click', function() {
+    // Redirige al enlace de la solicitud de archivos de Dropbox
+    window.location.href = "https://www.dropbox.com/request/6WWtWU73Yi1nycjWYoj1";
+});
